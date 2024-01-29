e0f5631a241e969a4fdfc6236c672ffe78505417
Pengli Shao <shao.pe@northeastern.edu>
2024-01-29 16:19:26 -0500

    first commit without addtional modification

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,16 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+// a function that creates button node
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
+    //In the for loop, let col2 be the createBtnNode
     tableNode.appendChild(createTRNode([col1]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
+
+//Create a new function called edit that can replace the first child of node to a input
