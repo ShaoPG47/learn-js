c79ef6e9f80d69f6307505256d6aaadf2a95f349
Pengli Shao <shao.pe@northeastern.edu>
2024-02-06 01:51:56 -0500

    reviewed commit

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,8 @@
-// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+
+
+// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg.
+function logMsg(obj) {
+    if (obj.hasOwnProperty('errMsg')) {
+        console.error(obj.errMsg)
+    }
+}
